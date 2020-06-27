033057ce5be8516ed9cbcbe3815bcafa480682e7
Peter Lukac <Peter@Peters-MacBook-Air.local>
2020-06-27 14:00:54 +0200

    Initial commit, all files from Dell PC

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import classes from './DrawerToggle.css';
+
+const drawerToggle = (props) => (
+	<div className={classes.DrawerToggle} 
+		 onClick={props.clicked}>MENU</div>
+);
+
+export default drawerToggle;
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+
+class ContactData extends Component {
+	state = {
+		name: '',
+		email: '',
+		address: {
+			street: '',
+			postalCode: ''
+		},
+		loading: false
+	}
+
+	orderHandler = (event) => {
+		event.preventDefault();
+		this.setState({loading: true});
+
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.price, //should be calculated on the server
+			customer: {
+				name: 'Peter X',
+				address: {
+					street: 'TestStreet 1',
+					zipCode: '41352',
+					country: 'Indonesia'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		}	
+		axios.post('/orders.json', order)
+			.then(response => {
+				// console.log(response))
+				this.setState({loading: false, purchasing: false});
+				this.props.history.push('/');
+
+			})
+			.catch(error => {
+				// console.log(response))
+				this.setState({loading: false, purchasing: false});
+			});
+	}
+
+	render () {
+		let form = (<form>
+					<input className={classes.Input} type="text" name="name" placeholder="Your name" />
+					<input className={classes.Input} type="email" name="email" placeholder="Your mail" />
+					<input className={classes.Input} type="text" name="street" placeholder="Street" />
+					<input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+					<Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+				</form>);
+		if (this.state.loading) {
+			form=<Spinner />;
+		}
+		return (
+			<div className={classes.ContactData}>
+				<h4>Enter your contact data</h4>
+				{ form }
+			</div>
+		);
+	}
+
+}
+
+export default ContactData;
