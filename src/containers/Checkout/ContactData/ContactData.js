c849fba43f9a7fb9907f263f8b05da396089f734
Peter Lukac <Peter@Peters-MacBook-Air.local>
2020-08-29 14:58:14 +0200

    Changed queryParams data passing to Redux #2. 280

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
@@ -95,6 +95,7 @@ class ContactData extends Component {
 	}
 
 	orderHandler = (event) => {
+
 		event.preventDefault();
 		this.setState({loading: true});
 		const formData = {};
@@ -102,7 +103,7 @@ class ContactData extends Component {
 			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
 		}
 		const order = {
-			ingredients: this.props.ingredients,
+			ingredients: this.props.ings,
 			price: this.props.price, //should be calculated on the server
 			orderData: formData,
 		}
@@ -197,4 +198,11 @@ class ContactData extends Component {
 
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+	return {
+			ings: state.ingredients,
+			price: state.totalPrice
+	}
+}
+
+export default connect(mapStateToProps)(ContactData);
