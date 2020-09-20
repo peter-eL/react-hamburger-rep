e695715a770bd2bd599dc3b886cc266c8bef98aa
Peter Lukac <Peter@Peters-MacBook-Air.local>
2020-09-20 10:51:44 +0200

    Redirect after purchase. 313

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import * as burgerBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 
 class BurgerBuilder extends Component {
@@ -40,6 +40,7 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseCancelHandler = () => {
+		this.props.onInitPurchase();
 		this.setState({purchasing: false});
 	}
 
@@ -106,9 +107,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 		return {
-				onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-				onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-				onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
+				onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+				onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+				onInitIngredients: () => dispatch(actions.initIngredients()),
+				onInitPurchase: () => dispatch(actions.purchaseInit())
 		}
 }
 
