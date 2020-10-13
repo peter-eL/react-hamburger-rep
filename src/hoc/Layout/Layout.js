edbf96bff3088be3f4e6623bb139cd3fd5d6ce52
Peter <peter@abc.com>
2020-10-13 16:45:38 +0300

    updating UI depending on login state. 332

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -24,8 +26,13 @@ class Layout extends Component {
 	render () {
 		return (
 	<Aux>
-		<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-		<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+		<Toolbar
+		 		isAuth={this.props.isAuthenticated}
+				drawerToggleClicked={this.sideDrawerToggleHandler}/>
+		<SideDrawer
+				isAuth={this.props.isAuthenticated}
+				open={this.state.showSideDrawer}
+				closed={this.sideDrawerClosedHandler}/>
 		<main className={classes.Content}>
 			{this.props.children}
 		</main>
@@ -35,4 +42,10 @@ class Layout extends Component {
 
 }
 
-export default Layout;
+const mapStateToProps = state => {
+		return {
+			isAuthenticated: state.auth.token !== null
+		};
+};
+
+export default connect(mapStateToProps) (Layout);
