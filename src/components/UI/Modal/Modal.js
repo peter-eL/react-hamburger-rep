679588536e02f255223cf510f4dea30559cee0d0
Peter <peter@abc.com>
2020-10-23 11:54:27 +0300

    BI, OS and Modal changed to func. React memo. 461

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React, { } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-		    
-	shouldComponentUpdate(nextProps, nextState) {
-		return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-	}
+const modal = props => {
 
-	// componentWillUpdate () {
-	// 	console.log('[Modal] component will update');
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	return nextProps.show !== props.show || nextProps.children !== props.children;
 	// }
 
-	render() {
-
-		return (
-		    <Aux>
-				<Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-				<div 
-					className={classes.Modal}
-					style={
-						{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-						 opacity: this.props.show ? '1':'0'
+	return (
+		<Aux>
+			<Backdrop show={props.show} clicked={props.modalClosed} />
+			<div
+				className={classes.Modal}
+				style={
+					{
+						transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+						opacity: props.show ? '1' : '0'
 					}}>
-						{this.props.children}
-				</div>
-			</Aux>
-		)
-	}
-} 
+				{props.children}
+			</div>
+		</Aux>
+	);
+};
 
-export default Modal;
+const updateProps = (prevProps, nextProps) => {
+	return nextProps.show === prevProps.show && nextProps.children === prevProps.children;
+};
+
+export default React.memo(modal, updateProps);
